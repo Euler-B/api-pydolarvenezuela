a736c556eb9f874a19a7bbbcb32f085f5c86e47d
Francisco Griman <grihardware@gmail.com>
2024-11-01 15:18:08 -0400

    Add Dropbox storage integration for file uploads

---
+++ src/storage/dropbox.py
@@ -0,0 +1,20 @@
+import dropbox
+from datetime import datetime
+from ..consts import DROPBOX_TOKEN, DROPBOX_APP_KEY, DROPBOX_APP_SECRET, DROPBOX_FOLDER
+
+class DropboxStorage:
+    def __init__(self):
+        self.client = dropbox.Dropbox(
+            DROPBOX_TOKEN, 
+            app_key=DROPBOX_APP_KEY, 
+            app_secret=DROPBOX_APP_SECRET
+        )
+
+        folders = self.client.files_list_folder('').entries
+        if DROPBOX_FOLDER not in [folder.name for folder in folders]:
+            self.client.files_create_folder(f'/{DROPBOX_FOLDER}')
+
+    def upload(self, file: str):
+        date_str = datetime.now().strftime('%Y-%m-%d')
+        with open(file, 'rb') as f:
+            self.client.files_upload(f.read(), f'/{DROPBOX_FOLDER}/backup_{date_str}.sql')
