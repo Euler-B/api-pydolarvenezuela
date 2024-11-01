f5d76f4f330654e460453bc251cd6468b7c9134a
Francisco Griman <grihardware@gmail.com>
2024-10-31 21:53:01 -0400

    Add timestamp to backup file name for better organization

--- src/backup.py
+++ src/backup.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from datetime import datetime
 from typing import Dict, Any
 from .consts import SQL_USER, SQL_HOST, SQL_DB_NAME, SQL_PASSWORD
 
@@ -7,11 +8,13 @@ if os.getenv('PGPASSWORD') is None:
     os.environ['PGPASSWORD'] = SQL_PASSWORD
 
 os.makedirs(os.path.join(os.path.dirname(__file__), 'tmp'), exist_ok=True)
-_BACKUP_PATH = os.path.join(
-    os.path.dirname(__file__), 'tmp', 'backup.sql'
-)
 
 def backup() -> Dict[str, Any]:
+    datetime_str = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    _BACKUP_PATH = os.path.join(
+        os.path.dirname(__file__), 'tmp', f'backup_{datetime_str}.sql'
+    )
+
     try:
         subprocess.run(
             ['pg_dump', '-h', SQL_HOST, '-U', SQL_USER, SQL_DB_NAME, '-f', _BACKUP_PATH]
