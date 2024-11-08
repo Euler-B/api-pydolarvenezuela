9e2120d0162a1cd6209e4f7c4e4b02ca3a12380d
Francisco Griman <grihardware@gmail.com>
2024-11-08 13:26:35 -0400

    Refactor exception handling to use HTTPException dataclass for improved clarity and consistency

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,17 +1,43 @@
+from typing import Optional
 from flask import jsonify
+from werkzeug.http import HTTP_STATUS_CODES
+from dataclasses import dataclass
 
-def forbidden(e):
-    message = jsonify({"error": "El servidor entendió la solicitud, pero se niega a autorizarla."}), 403
-    return message
+@dataclass
+class HTTPException(Exception):
+    code: Optional[int] = 500
+    message: Optional[str] = "Unknown Error"
+    response: Optional[str] = None
+
+    def __post_init__(self):
+        self.response = HTTP_STATUS_CODES.get(self.code, "Unknown Error").upper()
+
+def handle_http_exception(e: HTTPException):
+    return jsonify(
+        {
+            "error": e.response, 
+            "message": e.message
+        }
+    ), e.code
+
+# handle default exceptions
 
 def page_not_found(e):
-    message = jsonify({"error": "No se pudo encontrar la página que estaba buscando. Por favor, consulta la documentación en: https://github.com/fcoagz/api-pydolarvenezuela"}), 404
-    return message
+    result = HTTPException(404, "No se pudo encontrar la página que estaba buscando. Por favor, consulta la documentación en: https://github.com/fcoagz/api-pydolarvenezuela")
+    return handle_http_exception(result)
+
+def forbidden(e):
+    result = HTTPException(403, "El servidor entendió la solicitud, pero se niega a autorizarla.")
+    return handle_http_exception(result)
+
+def method_not_allowed(e):
+    result = HTTPException(405, "El método de solicitud no es compatible con la funcionalidad de la página.")
+    return handle_http_exception(result)
 
 def internal_server_error(e):
-    message = jsonify({"error": "Ha ocurrido un problema interno en el servidor. Por favor, inténtelo de nuevo más tarde."}), 500
-    return message
+    result = HTTPException(500, "Error interno del servidor.")
+    return handle_http_exception(result)
 
 def gateway_timeout(e):
-    message = jsonify({"error": "El servidor estaba actuando como una puerta de enlace o proxy y no recibió una respuesta a tiempo del servidor aguas arriba."}), 504
-    return message
+    result = HTTPException(504, "El servidor no pudo responder a tiempo.")
+    return handle_http_exception(result)
