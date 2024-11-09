18e2e17da597283b1aa151c6edeeb4c1bc688bb1
Francisco Griman <grihardware@gmail.com>
2024-11-09 11:05:20 -0400

    Add user database service with CRUD functionalities

---
+++ src/data/services/users_db.py
@@ -0,0 +1,34 @@
+import secrets
+from datetime import datetime
+from sqlalchemy.orm import Session
+from ..models import User
+from ..schemas import UserSchema
+
+user_schema = UserSchema()
+
+def is_user_valid(session: Session, token: str) -> bool:
+    user = session.query(User).filter(User.token == token).first()
+    if not user:
+        return False
+    return True
+
+def create_user(session: Session, name: str) -> str:
+    token = f'Bearer {secrets.token_urlsafe(16)}'
+    session.add(User(name=name, token=token, is_premium=True, created_at=datetime.now()))
+    session.commit()
+
+    return token
+
+def modificate_user(session: Session, id: int, is_premium: bool) -> None:
+    session.query(User).filter(User.id == id).update({
+        "is_premium": is_premium
+    })
+    session.commit()
+
+def delete_user(session: Session, id: int) -> None:
+    session.query(User).filter(User.id == id).delete()
+    session.commit()
+
+def get_users(session: Session) -> list:
+    models = session.query(User).all()
+    return user_schema.dump(models, many=True)
