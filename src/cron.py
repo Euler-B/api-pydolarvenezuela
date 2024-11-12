950c7bff99e7cd56c058a6617be1689c1ec1a3b2
Francisco Griman <grihardware@gmail.com>
2024-11-12 00:23:37 -0400

    Refactor monitor initialization to use Provider class and simplify database connection parameters

--- src/cron.py
+++ src/cron.py
@@ -8,21 +8,16 @@ from pyDolarVenezuela.pages import (
     EnParaleloVzla, 
     Italcambio
 )
-from pyDolarVenezuela import Monitor, Database, CheckVersion
+from pyDolarVenezuela import Monitor, CheckVersion
 from .core import logger
 from .core import cache
 from .consts import (
-    SQL_HOST,
-    SQL_MOTOR,
-    SQL_DB_NAME,
-    SQL_PORT,
-    SQL_USER,
-    SQL_PASSWORD,
     TIME_ZONE,
     CURRENCIES,
     PROVIDERS,
     UPDATE_SCHEDULE
 )
+from ._provider import Provider
 from .backup import backup
 from .storage.dropbox import DropboxStorage
 from .storage.telegram import TelegramStorage
@@ -30,9 +25,11 @@ from .storage.telegram import TelegramStorage
 CheckVersion.check = False
 
 pages    = [AlCambio, BCV, CriptoDolar, DolarToday, EnParaleloVzla, Italcambio]
-monitors = [Monitor(page, currency, db=Database(
-    SQL_MOTOR, SQL_HOST, SQL_DB_NAME, SQL_PORT, SQL_USER, SQL_PASSWORD
-    )) for currency in CURRENCIES.values() for page in pages if currency in page.currencies]
+monitors = [
+    Monitor(page, currency) 
+    for currency in CURRENCIES.values() 
+    for page in pages if currency in page.currencies
+]
 
 def update_data(name: str, monitor: Monitor) -> None:
     """
@@ -41,11 +38,12 @@ def update_data(name: str, monitor: Monitor) -> None:
     - name: Nombre del proveedor.
     - monitor: Instancia de Monitor.
     """
-    try:
-        cache.set(f'{name}:{monitor.currency}', json.dumps(
-            [m.__dict__ for m in monitor.get_all_monitors()], default=str))
-    except Exception as e:
-        logger.warning(f'Error al obtener datos de {monitor.provider.name}: {str(e)}')
+    # try:
+    provider = Provider(monitor.provider, monitor.currency, monitor.get_all_monitors())
+    cache.set(f'{name}:{monitor.currency}', json.dumps(
+        [m.__dict__ for m in provider.get_list_monitors()], default=str))
+    # except Exception as e:
+    #     logger.warning(f'Error al obtener datos de {monitor.provider.name}: {str(e)}')
 
 def reload_monitors() -> None:
     """
