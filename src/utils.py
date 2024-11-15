0abe181a1f7c75de2a8276d5f0ce75312db29503
Francisco Griman <grihardware@gmail.com>
2024-11-15 15:49:39 -0400

    Add get_provider function to retrieve provider key from PROVIDERS

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import httpx
 from uuid import uuid4
 from typing import Optional
+from .consts import PROVIDERS
 
 async def send_webhook(url: str, token: str, verify: bool, data: Optional[dict] = {'message': 'Hello, World!'}) -> None:
     try:
@@ -17,3 +18,8 @@ async def send_webhook(url: str, token: str, verify: bool, data: Optional[dict] 
         raise Exception(f'Error al enviar el webhook: {e}')
     except Exception as e:
         raise e
+    
+def get_provider(provider: str) -> str:
+    for key, value in PROVIDERS.items():
+        if provider.lower() in value.lower():
+            return key
