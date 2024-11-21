18df583ef1bafe7d32016b657fc7e34993dcb60d
Francisco Griman <grihardware@gmail.com>
2024-11-21 14:08:03 -0400

    fix: replace sessionmaker with context manager for session handling in send_webhooks function

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import httpx
 import asyncio
 from uuid import uuid4
 from typing import Optional
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from .data.engine import engine
 from .core import logger
 from .consts import PROVIDERS, CURRENCIES
@@ -40,65 +40,65 @@ def send_webhooks(test: bool = False, **kwargs) -> None:
     from .data.services.monitors_db import get_monitor_by_id as _get_monitor_by_id_
     from .data.services.webhooks_db import get_webhook_by_model as get_webhook
 
-    session = sessionmaker(bind=engine)()
-
     def send_webhook_test() -> None:
-        webhook = get_webhook(session, kwargs.get('token_user'))
-        if not webhook:
-            return
-        
-        data = []
-        for m in webhook.monitors:
-            monitor = _get_monitor_by_id_(session, m.monitor_id)
-            data.append(MonitorSchema().dump(monitor))
+        with Session(engine) as session:               
+            webhook = get_webhook(session, kwargs.get('token_user'))
+            if not webhook:
+                return
+            
+            data = []
+            for m in webhook.monitors:
+                monitor = _get_monitor_by_id_(session, m.monitor_id)
+                data.append(MonitorSchema().dump(monitor))
 
-        try:
-            asyncio.run(
-                send_webhook(webhook.url, webhook.token, webhook.certificate_ssl, {'monitors': data})
-            )
-        except Exception as e:
-            logger.error(f'Error al enviar el webhook: {str(e)}')
+            try:
+                asyncio.run(
+                    send_webhook(webhook.url, webhook.token, webhook.certificate_ssl, {'monitors': data})
+                )
+            except Exception as e:
+                logger.error(f'Error al enviar el webhook: {str(e)}')
 
     if test:
         send_webhook_test()
         return
 
-    monitor_webhooks = get_all_monitor_webhook()
-    if not monitor_webhooks:
-        return
-    
-    monitors_ids_save = {}
-    for webhook in get_all_webhooks(session):
-        if not webhook.status:
-            continue
-        data = []
-
-        for m in webhook.monitors:
-            if m.monitor_id not in monitor_webhooks:
+    with Session(engine) as session:
+        monitor_webhooks = get_all_monitor_webhook()
+        if not monitor_webhooks:
+            return
+        
+        monitors_ids_save = {}
+        for webhook in get_all_webhooks(session):
+            if not webhook.status:
                 continue
-            
-            if m.monitor_id in monitors_ids_save:
+            data = []
+
+            for m in webhook.monitors:
+                if m.monitor_id not in monitor_webhooks:
+                    continue
+                
+                if m.monitor_id in monitors_ids_save:
+                    data.append(monitors_ids_save[m.monitor_id])
+                    continue
+
+                monitor = _get_monitor_by_id_(session, m.monitor_id)
+                monitors_ids_save[m.monitor_id] = MonitorSchema().dump(monitor)
                 data.append(monitors_ids_save[m.monitor_id])
-                continue
+            try:
+                asyncio.run(
+                    send_webhook(webhook.url, webhook.token, webhook.certificate_ssl, {'monitors': data})
+                )
+            except Exception as e:
+                logger.error(f'Error al enviar el webhook: {str(e)}')
 
-            monitor = _get_monitor_by_id_(session, m.monitor_id)
-            monitors_ids_save[m.monitor_id] = MonitorSchema().dump(monitor)
-            data.append(monitors_ids_save[m.monitor_id])
-        try:
-            asyncio.run(
-                send_webhook(webhook.url, webhook.token, webhook.certificate_ssl, {'monitors': data})
-            )
-        except Exception as e:
-            logger.error(f'Error al enviar el webhook: {str(e)}')
+                if not is_intents_webhook_limit(webhook.id):
+                    set_webhook_status(webhook.id, 1)
+                else:
+                    change_webhook_status(session, webhook.id, False)
+                    delete_webhook_status(webhook.id)
+                    logger.info(f'Webhook desactivado por superar el límite de intentos: {webhook.url}')
 
-            if not is_intents_webhook_limit(webhook.id):
-                set_webhook_status(webhook.id, 1)
-            else:
-                change_webhook_status(session, webhook.id, False)
-                delete_webhook_status(webhook.id)
-                logger.info(f'Webhook desactivado por superar el límite de intentos: {webhook.url}')
-
-    delete_all_monitor_webhook()
+        delete_all_monitor_webhook()
 
 def get_provider(provider: str) -> str:
     for key, value in PROVIDERS.items():
