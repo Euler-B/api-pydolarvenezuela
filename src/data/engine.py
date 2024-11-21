0f4328c8e5c16b7aa8d62e3789edce6506c8bfa6
Francisco Griman <grihardware@gmail.com>
2024-11-21 11:49:06 -0400

    feat: increase SQLAlchemy engine pool size and max overflow for improved performance

--- src/data/engine.py
+++ src/data/engine.py
@@ -2,5 +2,5 @@ from sqlalchemy import create_engine
 from ..consts import URL_DB
 from .models import Base
 
-engine = create_engine(URL_DB)
+engine = create_engine(URL_DB, pool_size=20, max_overflow=20)
 Base.metadata.create_all(engine)
