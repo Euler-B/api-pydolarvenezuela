296ab5d19fe84eed0a669d87e19ccadb48d804d4
Francisco Griman <grihardware@gmail.com>
2024-11-29 03:19:04 -0400

    fix(Home.js): enhance description for clarity on API usage and target audience

--- src/www/static/components/Home.js
+++ src/www/static/components/Home.js
@@ -13,7 +13,7 @@ export default {
         <div class="max-w-4xl mx-auto text-center">
             <h2 class="text-3xl font-bold mb-4">Bienvenido a la API pyDolarVenezuela</h2>
             <p class="text-lg mb-6">
-            La API pyDolarVenezuela proporciona datos actualizados sobre el valor del dólar en Venezuela. Es una herramienta esencial para desarrolladores, economistas, y cualquier persona interesada en la economía venezolana.
+            La API pyDolarVenezuela proporciona datos actualizados sobre el valor del dólar en Venezuela. Es una herramienta esencial para desarrolladores que necesitan información precisa y actualizada sobre el valor del dólar en Venezuela.
             </p>
             <div class="flex justify-center space-x-4">
                 <a href="https://github.com/fcoagz/api-pydolarvenezuela" target="_blank" class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-800 transition duration-300">
