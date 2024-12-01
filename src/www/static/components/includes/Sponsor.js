955256b15379bb271816bdb3cb5fb46049fcff8c
Francisco Griman <grihardware@gmail.com>
2024-12-01 01:57:23 -0400

    feat(Sponsor.js): enhance sponsor section layout and add transition effects

--- src/www/static/components/includes/Sponsor.js
+++ src/www/static/components/includes/Sponsor.js
@@ -8,23 +8,23 @@ export default {
     },
     name: 'SponsorComponent',
     template: `
-        <section class="bg-blue-600 text-white p-4">
-            <h1 class="text-3xl font-bold">Sponsors</h1>
-            <div class="max-w-4xl mx-auto flex flex-wrap justify-center items-center">
-                <p class="text-center w-full sm:w-auto mr-2">Este proyecto es posible gracias a nuestros patrocinadores:</p>
-                <div class="w-full sm:w-auto mt-4 sm:mt-0 flex items-center">
-                    <ul class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-6">
-                        <li v-for="(sponsor, index) in sponsors" :key="index">
-                            <a :href="sponsor.url" target="_blank" class="flex items-center text-white" v-if="index < 3">
-                                <img :src="sponsor.logo" :alt="sponsor.name" class="w-16 h-16 rounded-full mr-2 bg-white"> -  {{ sponsor.description }}
-                            </a>
-                        </li>
-
-                        <li v-if="sponsors.length > 3">
-                            <a href="https://github.com/fcoagz/api-pydolarvenezuela#sponsors" class="text-white">Ver más...</a>
-                    </ul>
-                </div>
-            </div>
-        </section>
+  <section class="bg-blue-600 text-white p-4">
+    <div class="max-w-4xl mx-auto">
+      <h1 class="text-3xl font-bold text-center">Sponsors</h1>
+      <p class="text-center w-full mt-2 mb-4">Este proyecto es posible gracias a nuestros patrocinadores:</p>
+      <div class="flex flex-wrap justify-center items-center">
+        <transition-group name="fade" tag="div" class="flex flex-wrap justify-center items-center w-full">
+          <div v-for="(sponsor, index) in sponsors" v-show="index < 3" :key="index" class="m-4 p-4 text-black shadow-lg flex items-center justify-center w-24 h-24 animate-fade-in">
+              <a :href="sponsor.url" target="_blank" class="flex flex-col items-center">
+                <img :src="sponsor.logo" :alt="sponsor.name" class="w-full h-full mb-2 object-contain">
+              </a>
+          </div>
+        </transition-group>
+        <p v-if="sponsors.length > 3" class="text-white mt-4">
+          <a href="https://github.com/fcoagz/api-pydolarvenezuela#sponsors" class="underline">Ver más...</a>
+        </p>
+      </div>
+    </div>
+  </section>
     `
 }
