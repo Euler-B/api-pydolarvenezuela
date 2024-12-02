0d2f969895911c9f3f6b088c2038f288b56a2cda
Francisco Griman <grihardware@gmail.com>
2024-12-02 17:30:51 -0400

    feat(index.py): add exception handling to index route

--- src/routes/index.py
+++ src/routes/index.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from ..decorators import handle_exceptions
 
 route = Blueprint('index', __name__)
 
 @route.get('/', defaults={'path': ''})
 @route.get('/<path:path>')
-def index(path: str):
+@handle_exceptions
+def index(path: str = None):
     return render_template('index.html')
