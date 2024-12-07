817b5ec7b45338180f5218d37ab6363be95b3906
Francisco Griman <grihardware@gmail.com>
2024-12-07 02:49:14 -0400

    refactor(webhooks_db.py): remove caching functions and update import paths for clarity

--- src/data/services/webhooks_db.py
+++ src/data/services/webhooks_db.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from sqlalchemy import func, distinct
 from sqlalchemy.orm import Session
 from ...exceptions import MissingKeyError, WebhookExistsError
-from ...core import cache
 from ..models import User, Webhook, MonitorsWebhooks, Page, Currency, Monitor
 from ..schemas import WebhookSchema
 from .users_db import is_user_valid
@@ -17,7 +16,7 @@ def raise_webhook_exists_error(session: Session, token_user: str) -> None:
         raise WebhookExistsError('El webhook ya existe')
     
 def create_webhook(session: Session, token_user: str, **kwargs) -> None:
-    from ...utils import get_provider, get_currency    
+    from ...utils.func_consts import get_provider, get_currency    
     user = session.query(User).filter(User.token == token_user).first()
 
     try:
@@ -106,36 +105,3 @@ def get_unique_monitor_ids(session: Session) -> List[int]:
 
 def get_all_webhooks(session: Session) -> List[Webhook]:
     return session.query(Webhook).all()
-
-# Cache
-
-def get_all_monitor_webhook() -> List[int]:
-    keys = cache.keys('monitor_webhook:*')
-    if not keys:
-        return []
-
-    return [int(key.split(':')[1]) for key in keys if cache.get(key) not in [None, f'{int(False)}']]
-
-def set_monitor_webhook(monitor_id: int, boolean: bool) -> None:
-    cache.set(f'monitor_webhook:{monitor_id}', int(boolean))
-
-def is_monitor_webhook(monitor_id: int) -> bool:
-    return cache.get(f'monitor_webhook:{monitor_id}') is not None
-
-def delete_monitor_webhook(monitor_id: int) -> None:
-    cache.delete(f'monitor_webhook:{monitor_id}')
-
-def delete_all_monitor_webhook() -> None:
-    keys = cache.keys('monitor_webhook:*')
-    for key in keys:
-        set_monitor_webhook(int(key.split(':')[1]), False)
-
-def set_webhook_status(webhook_id: int, intents: int) -> None:
-    intents_c = int(cache.get(f'webhook_status:{webhook_id}')) if cache.get(f'webhook_status:{webhook_id}') else 0
-    cache.set(f'webhook_status:{webhook_id}', intents_c + intents)
-
-def is_intents_webhook_limit(webhook_id: int) -> bool:
-    return int(cache.get(f'webhook_status:{webhook_id}')) == 3 if cache.get(f'webhook_status:{webhook_id}') else False
-
-def delete_webhook_status(webhook_id: int) -> None:
-    cache.delete(f'webhook_status:{webhook_id}')
