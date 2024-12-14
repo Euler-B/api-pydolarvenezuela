6da8454e8e86676d5fdc1a09424ce70b6d41baba
Francisco Griman <grihardware@gmail.com>
2024-12-14 02:28:46 -0400

    feat(routes): add rounded_price parameter to monitor retrieval functions

--- src/routes/monitors.py
+++ src/routes/monitors.py
@@ -18,8 +18,9 @@ def get_monitor_by_page_or_monitor(currency: Literal['dollar', 'euro']):
     page    = request.args.get('page')
     monitor = request.args.get('monitor')
     format_date = request.args.get('format_date', 'default')
+    rounded_price = True if request.args.get('rounded_price', 'true') == 'true' else False
 
-    response = get_page_or_monitor(currency, page, monitor, format_date)
+    response = get_page_or_monitor(currency, page, monitor, format_date, rounded_price)
         
     return jsonify(response), 200
 
@@ -32,6 +33,7 @@ def get_history(currency: Literal['dollar', 'euro']):
     start_date = request.args.get('start_date')
     end_date   = request.args.get('end_date')
     format_date = request.args.get('format_date', 'default')
+    rounded_price = True if request.args.get('rounded_price', 'true') == 'true' else False
 
     if not all([page, monitor, start_date, end_date]):
         raise ValueError('Por favor, proporciona los parametros: (page, monitor, start_date y end_date).')
@@ -39,7 +41,7 @@ def get_history(currency: Literal['dollar', 'euro']):
     for date in [start_date, end_date]:
         _validate_date(date)
 
-    response = get_history_prices(currency, page, monitor, start_date, end_date, format_date)
+    response = get_history_prices(currency, page, monitor, start_date, end_date, format_date, rounded_price)
     return jsonify(response), 200
 
 @route.get('/<string:currency>/changes')
@@ -50,13 +52,14 @@ def get_daily_changes(currency: Literal['dollar', 'euro']):
     monitor = request.args.get('monitor')
     date = request.args.get('date')
     format_date = request.args.get('format_date', 'default')
+    rounded_price = True if request.args.get('rounded_price', 'true') == 'true' else False
 
     if not all([page, monitor, date]):
         raise ValueError('Por favor, proporciona los parametros: (page, monitor y date).')
     
     _validate_date(date)
 
-    response = get_daily_changes_(currency, page, monitor, date, format_date)
+    response = get_daily_changes_(currency, page, monitor, date, format_date, rounded_price)
     return jsonify(response), 200
 
 @route.get('/<string:currency>/conversion')
