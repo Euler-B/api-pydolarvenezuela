c2401197b02977304f6add0f83ef27eb19212339
Francisco Griman <grihardware@gmail.com>
2024-12-20 03:52:58 -0400

    feat(user): add endpoint to change user name with token validation

--- src/routes/user.py
+++ src/routes/user.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from sqlalchemy.orm import Session
 from ..data.engine import engine
 from ..decorators import token_required, handle_exceptions
-from ..data.services.users_db import get_user_id, get_user, is_user_valid
+from ..data.services.users_db import get_user_id, get_user, is_user_valid, change_user_name
 
 route = Blueprint('users', __name__)
 
@@ -23,3 +23,18 @@ def get_user_data():
     with Session(engine) as session:
         user_id = get_user_id(session, token)
         return jsonify(get_user(session, user_id))
+    
+@route.put('/change-name')
+@token_required
+@handle_exceptions
+def change_name():
+    token = request.headers.get('Authorization')
+    name = request.get_json().get('name')
+
+    if not name:
+        raise ValueError('name es requerido')
+    
+    with Session(engine) as session:
+        user_id = get_user_id(session, token)
+        change_user_name(session, user_id, name)
+        return jsonify({'success': True})
