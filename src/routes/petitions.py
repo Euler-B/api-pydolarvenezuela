a4dcf698e2e1cee9c75a35576bc18f883c583048
Francisco Griman <grihardware@gmail.com>
2024-12-30 10:11:26 -0400

    feat(petitions): refactor hourly and daily totals endpoints to use a common caching function for improved code reuse

--- src/routes/petitions.py
+++ src/routes/petitions.py
@@ -1,6 +1,8 @@
+from typing import Callable
 from flask import Blueprint, request, jsonify
 from sqlalchemy.orm import Session
 from ..data.engine import engine
+from ..utils.cache import CacheHistoryPetition
 from ..decorators import token_required, handle_exceptions
 from ..data.services.user_petitions_db import (
     get_hourly_totals_24h,
@@ -10,29 +12,37 @@ from ..data.services.user_petitions_db import (
 
 route = Blueprint('petitions', __name__)
 
+def _get_hourly_or_daily_totals(func: Callable, path: str, token: str) -> list:
+    cache = CacheHistoryPetition(path.split('/')[-1].replace('-', '_'), token.split(' ')[1])
+    if cache.get(): return cache.get()
+
+    with Session(engine) as session:
+        cache.set(func(session, token))
+    return cache.get()
+
 @route.get('/hourly-totals-24h')
 @token_required
 @handle_exceptions
 def hourly_totals_24h():
-    token_user = request.headers.get('Authorization')
+    token = request.headers.get('Authorization')
 
-    with Session(engine) as session:
-        return jsonify(get_hourly_totals_24h(session, token_user))
+    response = _get_hourly_or_daily_totals(get_hourly_totals_24h, request.path, token)
+    return jsonify(response)
     
 @route.get('/daily-totals-7d')
 @token_required
 @handle_exceptions
 def daily_totals_7d():
-    token_user = request.headers.get('Authorization')
+    token = request.headers.get('Authorization')
 
-    with Session(engine) as session:
-        return jsonify(get_daily_totals_7d(session, token_user))
+    response = _get_hourly_or_daily_totals(get_daily_totals_7d, request.path, token)
+    return jsonify(response)
     
 @route.get('/daily-totals-30d')
 @token_required
 @handle_exceptions
 def daily_totals_30d():
-    token_user = request.headers.get('Authorization')
+    token = request.headers.get('Authorization')
 
-    with Session(engine) as session:
-        return jsonify(get_daily_totals_30d(session, token_user))
+    response = _get_hourly_or_daily_totals(get_daily_totals_30d, request.path, token)
+    return jsonify(response)
