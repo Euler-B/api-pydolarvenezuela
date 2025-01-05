da908650876b0e2ef16499574cb58b9a461182ac
Francisco Griman <grihardware@gmail.com>
2025-01-05 16:12:57 -0400

    feat: add fallback message for missing index.html in API route

--- src/routes/index.py
+++ src/routes/index.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+import os
+from flask import Blueprint, render_template, jsonify
 from ..consts import ALLOWED_ROUTES
 from ..exceptions import HTTPException
 from ..decorators import handle_exceptions
@@ -9,6 +10,9 @@ route = Blueprint('index', __name__)
 @route.get('/<path:path>')
 @handle_exceptions
 def index(path: str = None):
+    if not os.path.exists(f'src/www/dist/index.html'):
+        return jsonify({'message': 'Welcome to PyDolarVenezuela API!'}), 200
+
     if path in ALLOWED_ROUTES:
         return render_template('index.html')
     raise HTTPException(404, "No se pudo encontrar la página que estaba buscando. Por favor, consulta la documentación en: https://github.com/fcoagz/api-pydolarvenezuela")
